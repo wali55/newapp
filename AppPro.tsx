5cf4804a81aad9ee77724fd2331160ae10de8add
wali55 <walisantunu@gmail.com>
2024-06-19 14:04:08 +0600

    style sheet

---
+++ AppPro.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {View, Text, StyleSheet, useColorScheme} from 'react-native';
+
+function AppPro(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  whiteText: {
+    color: '#fff'
+  },
+  darkText: {
+    color: '#000'
+  }
+});
+
+export default AppPro;
